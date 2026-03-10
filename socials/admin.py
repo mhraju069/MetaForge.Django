cc46b2d1df140d75c2a7dfa413a6c307d8819880
Raju <mhraju069@gmail.com>
2026-03-11 01:43:41 +0600

    add: updatye admin pannel

--- socials/admin.py
+++ socials/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import SocialAccount
+from .models import *
 from unfold.admin import ModelAdmin
 # Register your models here.
 
 admin.site.register(SocialAccount,ModelAdmin)
+admin.site.register(SocialPost,ModelAdmin)
+admin.site.register(PostMedia,ModelAdmin)
+
